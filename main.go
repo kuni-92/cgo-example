d3302b0ea963b9df5b55a9a7c4576fb82a390300
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2021-11-17 22:48:50 +0900

    translate C int to Go int

--- main.go
+++ main.go
@@ -13,7 +13,16 @@ func main() {
 	addres := C.add(10, 20)
 	fmt.Println("C.add result: ", addres)
 	divres := C.div(10, 20)
-	fmt.Println("C.add result: ", divres)
+	fmt.Println("C.div result: ", divres)
 	modres := C.mod(10, 20)
-	fmt.Println("C.add result: ", modres)
+	fmt.Println("C.mod result: ", modres)
+
+	numbers := []int{
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+	}
+	var sum int
+	for _, n := range numbers {
+		sum = int(C.add(C.int(sum), C.int(n)))
+	}
+	fmt.Println("sum 1 to 10 result: ", sum)
 }
